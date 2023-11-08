fe0cffef861f40774ceaee96cbbeeb327e799c21
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-11-08 20:24:10 +0600

    user booking section implemented  based on logged in user

--- src/Services/MySchedules/MySchedules.jsx
+++ src/Services/MySchedules/MySchedules.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
 
 const MySchedules = () => {
+    const {User} = useAuth()
+    const axios = useAxios()
+    const [bookingData , SetBookingData] = useState([])
+    useEffect(()=>{
+        axios(`allBookings?BookingUserEmail=${User?.email}`)
+        .then(res=> {
+        //    console.log(res.data)
+           SetBookingData(res.data)
+        })
+        .catch(
+            error => {
+                console.log(error)
+            }
+        )
+    },[])
+    // console.log(bookingData)
     return (
-        <div>
-            
+        <div className="max-w-screen-2xl mx-auto">
+            <div className="flex justify-center">
+            <section className="flex-1 bg-teal-500 h-full">
+                <h1 className="text-4xl text-white text-center p-2 underline">My Bookings</h1>
+
+          <div className="w-2/3 mx-auto bg-white mt-5 mb-5">
+          <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        
+        <th className="text-lg font-bold">Service Name</th>
+        <th  className="text-lg font-bold">Booking Date</th>
+        <th  className="text-lg font-bold">Booking ID</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+     {
+        bookingData?.map(booking =>  <tr key={booking?._id}>
+        
+            <td className="font-bold text-gray-900">{booking?.BookingServiceName}</td>
+            <td className="font-bold text-gray-900">{booking?.BookingDate}</td>
+            <td className="font-bold text-gray-900">{booking?._id}</td>
+          </tr>)
+     }
+  
+    </tbody>
+  </table>
+</div>
+          </div>
+
+            </section>
+            <section className="flex-1 bg-amber-500 h-full">
+                <h1 className="text-4xl text-white text-center  p-2 underline">My Pending Works</h1>
+            </section>
+            </div>
         </div>
     );
 };
